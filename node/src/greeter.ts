a084719b1bd670598ac0c888d2190ff6c34c9c3e
Aleksandr Sokolovskii <github@amsokol.io>
2020-06-12 13:08:24 +0300

    [Node.js] added linter
    Signed-off-by: Aleksandr Sokolovskii <github@amsokol.io>

--- node/src/greeter.ts
+++ node/src/greeter.ts
@@ -3,21 +3,28 @@ import * as grpc from 'grpc'
 import { HelloRequest, HelloReply } from './protogen/hello_pb'
 import { GreeterService, IGreeterServer } from './protogen/hello_grpc_pb'
 
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 const charactersLength = characters.length
 
-class GreeterHandler implements IGreeterServer {
+/**
+ * GreeterHandler is implementation of IGreeterServer.
+ *
+ */
+class GreeterHandler /* implements IGreeterServer */ {
     /**
      * Greet the user nicely
-     * @param call
-     * @param callback
+     * @param {grpc.ServerUnaryCall<HelloRequest>} call
+     * @param {grpc.sendUnaryData<HelloReply>} callback
      */
-    sayHello = (call: grpc.ServerUnaryCall<HelloRequest>, callback: grpc.sendUnaryData<HelloReply>): void => {
+    sayHello = (call: grpc.ServerUnaryCall<HelloRequest>,
+        callback: grpc.sendUnaryData<HelloReply>): void => {
         const reply: HelloReply = new HelloReply()
 
-        var len = Math.floor(Math.random() * 800 + 200)
-        var res = [...Array(len)].map((_, x) => String.fromCharCode(characters.charCodeAt(
-            Math.floor(Math.random() * charactersLength))))
+        const len = Math.floor(Math.random() * 800 + 200)
+        const res = [...Array(len)].map((_) =>
+            String.fromCharCode(characters.charCodeAt(
+                Math.floor(Math.random() * charactersLength))))
         reply.setMessage(res.join(''))
 
         callback(null, reply)
@@ -25,6 +32,6 @@ class GreeterHandler implements IGreeterServer {
 }
 
 export default {
-    service: GreeterService,                // Service interface
-    handler: new GreeterHandler(),          // Service interface definitions
+    service: GreeterService, // Service interface
+    handler: new GreeterHandler(), // Service interface definitions
 }
